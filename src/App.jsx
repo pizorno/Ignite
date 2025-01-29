4cf1f410ea2e92759fc16b1017315dd5adf22cd9
Eduardo <pizorno@gmail.com>
2025-01-29 20:06:32 -0300

    :sparkles: feat: Adding Sidebar component with some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Header } from './components/Header'
 import { Post } from './Posts'
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=50&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <div className={styles.profile}>
+                <strong>Eduardo Pizorno</strong>
+                <span>Web Developer</span>  
+            </div>
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    )
+}
