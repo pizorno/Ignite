12e697ba71f11ee3c8a3948b9afaa26ad63847ba
Eduardo <pizorno@gmail.com>
2025-01-30 08:32:25 -0300

    :lipstick: feat: Adding Sidebar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -5,11 +6,15 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=50&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/pizorno.png" />
                 <strong>Eduardo Pizorno</strong>
                 <span>Web Developer</span>  
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
